5a897e28c08c85d8664914c2a0d223ed071f6146
wiebie94 <34031465+wiebie94@users.noreply.github.com>
2022-05-30 17:13:30 +0200

    scripte umgebaut

---
+++ Assets/Scripts/FireballController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballController : MonoBehaviour
+{
+    private BurnController burnController;
+    public Material burnMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       burnController.GetComponent<BurnController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0,0,10) * Time.deltaTime;
+    }
+
+     private void OnTriggerEnter(Collider other) {
+         
+        //wenn objekt verbrennen soll
+        if(other.gameObject.tag == "Burnable"){
+            foreach(Transform child in other.gameObject.transform){
+                child.gameObject.AddComponent<BurnController>();   //add burnscript to childs
+            }
+            Debug.Log("burrrrrrrrrn");
+  
+        }
+
+        //wenn objekt etwas anzünden soll
+        if(other.gameObject.tag == "Kindle") {
+            Debug.Log("kindle stuff");
+        }
+
+
+        //immer
+        transform.gameObject.SetActive(false);  //feuerball löschen
+        
+        /*
+        *   explosion an der auftreffstelle
+        */
+    }
+}
