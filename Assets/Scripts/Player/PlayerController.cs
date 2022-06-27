aebde9f7c8410851941e4e324f985b5819d7d644
Sasha <alx.borko@gmail.com>
2022-06-28 01:35:27 +0200

    Improved Movement 0.1

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     private XROrigin xrRig;
     private CapsuleCollider collider;
     private Rigidbody rb;
+
+    [SerializeField] GameObject camObj;
+    
+    [SerializeField] InputActionProperty moveAction;
+    [SerializeField] float moveSpeed = 1;
+
+    [SerializeField] float velocityLimit = 50;
 
     void Start()
     {
@@ -18,8 +26,20 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Vector3 center = xrRig.CameraInOriginSpacePos;
-        collider.center = new Vector3(center.x, collider.center.y, center.z);
+        Debug.Log(rb.velocity.magnitude);
+
+        Vector2 moveVector = moveAction.action.ReadValue<Vector2>();
+
+        if (moveVector != Vector2.zero && rb.velocity.magnitude < velocityLimit)
+        {
+            Vector3 moveForce = camObj.transform.forward * moveVector.y * moveSpeed * Time.deltaTime;
+
+            rb.AddForce(moveForce);
+        }
+            
+
+        //Vector3 center = xrRig.CameraInOriginSpacePos;
+        //collider.center = new Vector3(center.x, collider.center.y, center.z);
         collider.height = xrRig.CameraInOriginSpaceHeight;
     }
 }
